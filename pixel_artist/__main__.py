6073a295d0efde9ac403c42b3efd976247c45409
Alexandre Pinto <alexandpinto@gmail.com>
2022-04-23 17:18:40 +0100

    documentation

--- pixel_artist/__main__.py
+++ pixel_artist/__main__.py
@@ -8,29 +8,29 @@ from .parser import build_parser, parse_args
 
 
 def main() -> None:
-	"""Parses the command line arguments and runs the tool."""
+    """Parses the command line arguments and runs the tool."""
 
-	arg_parser = build_parser()
-	args = parse_args(arg_parser)
+    arg_parser = build_parser()
+    args = parse_args(arg_parser)
 
-	pixel_art = PixelArt(args['filename'],
-						 args['granularity'],
-						 args['ncolors'],
-						 args['nbits'],
-						 args["color_space"],
-						 args['verbose'])
+    pixel_art = PixelArt(args['filename'],
+                         args['granularity'],
+                         args['ncolors'],
+                         args['nbits'],
+                         args['color_space'],
+                         args['verbose'])
 
-	pixelated_image = pixel_art.pixelate()
-	pixelated_image.show()
-	if args['save']:
-		if args['verbose']:
-			print("Saving to " + args['filename'].split(".")[0] + "_pixelated.png ...")
-			pixelated_image.save(args['filename'].split(".")[0] + "_pixelated.png")
-	if args['verbose']:
-		print('Done')
+    pixelated_image = pixel_art.pixelate()
+    pixelated_image.show()
+    if args['save']:
+        if args['verbose']:
+            print("Saving to " + args['filename'].split(".")[0] + "_pixelated.png ...")
+            pixelated_image.save(args['filename'].split(".")[0] + "_pixelated.png")
+    if args['verbose']:
+        print('Done')
 
-	return SUCCESS
+    return SUCCESS
 
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
